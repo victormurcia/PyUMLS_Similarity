0aafbae9edd4e05377ea5fb293f888bfb330f485
Victor Murcia <victor.murcia@wsu.edu>
2023-12-06 19:32:41 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
+project = 'PyUMLS-Similarity'
+copyright = '2023, Victor M. Murcia'
 author = 'Graziella'
 
 release = '0.1'
